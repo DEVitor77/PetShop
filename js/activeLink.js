67987983290ff1b9887e3b5ba4a4ea4a49254fbc
Valter Vitor <valtervitorfilho@gmail.com>
2023-07-17 18:51:07 -0300

    meu curso completo

---
+++ js/activeLink.js
@@ -0,0 +1,26 @@
+console.log('Active Link');
+
+let navBar = document.querySelectorAll('a.navbar-link');
+// let path = window.location.pathname;
+// console.dir(path);
+function deleteActive() {
+    navBar.forEach(function(element){
+        element.classList.remove('active');
+    });
+}
+deleteActive();
+navBar.forEach(function(element){
+    let text = element.href; 
+    let pattern = /#/;
+    let result = pattern.test(text);
+    if(result === true) {
+        element.classList.add('active');
+        element.style.backgroundColor = "#999999";
+    }
+});
+navBar.forEach(function(element){
+    element.addEventListener('click', function(event) {
+        deleteActive();
+        event.target.classList.add('active');
+    });
+});
