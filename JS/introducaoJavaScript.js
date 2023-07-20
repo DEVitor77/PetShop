5357d0744d6c055124fc74b7403debe5d1089d7f
valter vitor <valtervitorfilho@gmail.com>
2023-07-19 23:08:35 -0300

    att main

---
+++ JS/introducaoJavaScript.js
@@ -0,0 +1,50 @@
+// // console.log('meu arquivo importado');
+
+
+// let nome = 'Valter';
+// let sobreNome = 'Vitor';
+// const dataNascimento = '03/05/1972';
+
+
+// let nomeCompleto = `${nome} ${sobreNome} nascido em ${dataNascimento}`;
+
+
+
+// // console.log(nomeCompleto);
+// // console.log(dataNascimento);
+
+// let tipo = typeof nomeCompleto;
+
+// console.log(tipo);
+// console.log(nomeCompleto);
+
+// let media = 7;
+
+// if (media === 5) {
+//     console.log('Aprovado');
+// } else {
+//     console.log('Reprovado');
+// }
+
+// let compras = ['oleo', 'batata', 'sabão']
+// let lista = [compras, 'jonas', 'paulo', 14, 400, true];
+
+// compras.forEach((element, index, array) => {
+//     console.log(element);
+// });
+// debugger;
+
+// for (let i=0; i<3; i++){
+//     console.log(compras[1]);
+// }
+
+// funções
+function somaCompra(num1, num2){
+    let numero1 = num1;
+    let numero2 = num2;
+    let soma = numero1 + numero2;
+    console.log(soma);
+}
+
+somaCompra(50, 30);
+
